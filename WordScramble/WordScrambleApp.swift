b544824db548ec6e2167834f97557cb6a47a928b
Sergey Petrosyan <ipetriez@icloud.com>
2024-02-15 22:00:47 +0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Sergey Petrosyan on 15.02.24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
